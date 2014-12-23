77bbffe85939b6e34e8e24054c24d0140658c59d
Juan Puig <juan@layer.com>
2014-12-23 15:52:29 -0800

    lyr_metrics API extended with i/0, vm/0, getters, etc.

--- src/lyr_metrics.erl
+++ src/lyr_metrics.erl
@@ -14,9 +14,13 @@
 -export([start_link/0]).
 -export([new_counter/1]).
 -export([new_gauge/2]).
+-export([new_histogram/2]).
 -export([incr/1]).
--export([sample/1,
-         sample/2]).
+-export([get_gauges/0]).
+-export([get_counters/0]).
+-export([get_histograms/0]).
+-export([vm/0]).
+-export([i/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -24,11 +28,12 @@
 
 -define(SERVER, ?MODULE).
 -define(TIMEOUT, 1000).
--define(DEBUG(Fmt), lager:debug(Fmt)).
--define(DEBUG(Fmt, Args), lager:debug(Fmt, Args)).
+-define(LOG(Level,Fmt), lager:Level(Fmt)).
+-define(LOG(Level, Fmt, Args), lager:Level(Fmt, Args)).
 
 -record(state, {gauges = [],
-                counters = []}).
+                counters = [],
+                histograms = []}).
 
 %%%===================================================================
 %%% API
@@ -43,15 +48,29 @@ new_counter(Name) ->
 new_gauge(Name, {_Fun, _Args} = Callback) ->
     gen_server:call(?SERVER, {new, gauge, Name, Callback}).
 
+new_histogram(Name, {_Fun, _Args} = Callback) ->
+    gen_server:call(?SERVER, {new, histogram, Name, Callback}).
+
+get_gauges() ->
+    gen_server:call(?SERVER, {get, gauges}).
+
+get_counters() ->
+    gen_server:call(?SERVER, {get, counters}).
+
+get_histograms() ->
+    gen_server:call(?SERVER, {get, histograms}).
+
 -spec incr(list(atom())) -> ok | {error, not_found}.
 incr(Counter) ->
     gen_server:cast(?SERVER, {incr, Counter}).
 
-sample(Metric) ->
-    sample(Metric, metric_to_fun(Metric)).
+vm() ->
+    VMStats = exometer:get_values([erlang]),
+    [{Name, Value} || {Name, [{value, Value}, {ms_since_reset, _}]} <- VMStats].
 
-sample(_Metric, {Fun, Args}) ->
-    erlang:apply(Fun, Args).
+i() ->
+    gen_server:call(?SERVER, info).
+
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -60,36 +79,68 @@ sample(_Metric, {Fun, Args}) ->
 init([]) ->
     case application:get_env(exometer, predefined) of
         {ok, Predefined} ->
-            Gauges = [ Name || {Name, gauge, _Opt} <- Predefined ],
-            ?DEBUG("exometer gauges available ~p", [Gauges]),
-            {ok, #state{gauges=Gauges}, ?TIMEOUT};
+            Gauges = [ {Name, metric_to_fun(Name)} || {Name, gauge, _Opt} <- Predefined ],
+            ?LOG(info, "~p has configured ~p gauges", [?MODULE, length(Gauges)]),
+            Histograms = [ {Name, metric_to_fun(Name)} || {Name, histogram, _Opt} <- Predefined ],
+            ?LOG(info, "~p has configured ~p histograms", [?MODULE, length(Histograms)]),
+            Counters = [ Name || {Name, counter, _Opt} <- Predefined ],
+            ?LOG(info, "~p has configured ~p counters", [?MODULE, length(Counters)]),
+            {ok, #state{gauges=Gauges, histograms=Histograms, counters=Counters}, ?TIMEOUT};
         undefined ->
-            ?DEBUG("exometer has no available metrics"),
+            ?LOG(info, "~p has found no metrics", [?MODULE]),
             {ok, #state{}}
     end.
 
 handle_call({new, counter, Name}, _From, #state{counters = Counters} = State) ->
     Result = exometer:new(Name, counter),
-    ?DEBUG("New counter ~p", [Name]),
+    ?LOG(info, "~p has installed a new counter: ~p", [?MODULE, Name]),
     {reply, Result, State#state{counters = [Name | Counters]}};
-handle_call({new, gauge, Name, Callback}, _From, #state{gauges = Gauges} = State) ->
+handle_call({new, Type, Name, Callback}, _From, #state{gauges = G, histograms = H} = State) 
+  when Type =:= gauge; Type =:= histogram ->
     case is_callback(Callback) of
         true ->
-            ?DEBUG("New gauge ~p", [Name]),
-            ok = exometer:new(Name, gauge),
-            {reply, ok, State#state{gauges = [{Name, Callback} | Gauges]}};
+            ok = exometer:new(Name, Type),
+            NewState = 
+                case Type of
+                    gauge ->
+                        State#state{gauges = [{Name, Callback} | G]};
+                    histogram ->
+                        State#state{histograms = [{Name, Callback} | H]}
+                end,
+            ?LOG(info, "~p has installed a new ~p: ~p", [?MODULE, Type, Name]),
+            {reply, ok, NewState};
         false ->
             {reply, {error, badarg}, State}
-    end.
+    end;
+handle_call({get, Types}, _From, State) 
+  when Types =:= gauges; Types =:= histograms; Types =:= counters ->
+    Result = [ begin 
+                   Name = metric_name(Metric),
+                   {ok, Info} = exometer:get_value(Name),
+                   Value = proplists:get_value(value, Info, Info), 
+                   {Name, Value} 
+               end || Metric <- return(Types, State) ],
+    {reply, Result, State};
+handle_call(info, _From, #state{counters = C,
+                                histograms = H,
+                                gauges = G} = State) ->
+    Result = 
+        [{total, length(C ++ H ++ G)},
+         {counters, C},
+         {histograms, H},
+         {gauges, G}],
+    %% Add a field with the timer ref if available.
+    {reply, Result, State}.
+    
 
 handle_cast({incr, Name}, State) ->
-    ok = exometer:update(Name, 1),
+    exometer:update(Name, 1), %% add pattern match
     {noreply, State}.
 
-handle_info(timeout, #state{gauges = []} = State) ->
+handle_info(timeout, #state{gauges = [], histograms = []} = State) ->
     {noreply, State};
-handle_info(timeout, #state{gauges = Gauges} = State) ->
-    true = exometer_update(Gauges),
+handle_info(timeout, #state{gauges = G, histograms = H} = State) ->
+    true = update_metrics(G ++ H),
     {noreply, State, ?TIMEOUT}.
 
 terminate(_Reason, _State) ->
@@ -102,10 +153,33 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
-exometer_update(Metrics) ->
-    Results = [ok = exometer:update(Metric,
+update_metrics(Metrics) ->
+    Results = [ok = exometer:update(metric_name(Metric),
                                     sample(Metric)) || Metric <- Metrics],
     lists:all(fun(ok) -> true; (_) -> false end, Results).
+
+sample(Metric) ->
+    sample(Metric, metric_callback(Metric)).
+
+sample(Metric, {Fun, Args}) ->
+    Result = erlang:apply(Fun, Args),
+    ?LOG(debug, "Sampled ~p with value: ~p", [Metric, Result]),
+    Result.
+
+return(counters, #state{counters = Counters}) ->
+    Counters;
+return(gauges, #state{gauges = Gauges}) ->
+    Gauges;
+return(histograms, #state{histograms = Histograms}) ->
+    Histograms.
+
+metric_name({Name, _Callback}) ->
+    Name;
+metric_name(Name) ->
+    Name.
+
+metric_callback({_Name, Callback}) ->
+    Callback.    
 
 metric_to_fun([erlang, processes]) ->
     {fun erlang:memory/1, [processes]};
